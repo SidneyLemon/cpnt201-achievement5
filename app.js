b18e4ceb9b889e4dbc45af700e6fc92f626fd531
Sidney Lemon <sid.lemon3@gmail.com>
2023-02-15 12:27:04 -0700

    initial

---
+++ app.js
@@ -0,0 +1,8 @@
+const dayjs = require('dayjs');
+
+const date1 = "2020-01-01";
+console.log(date1);
+const date2 = "2021-02-02";
+console.log(date2);
+const date3 = "2022-03-03";
+console.log(date3);
